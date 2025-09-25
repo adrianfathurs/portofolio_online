19388f43f2587522cc81fb720bac8b64d7027c99
Adrian Fathur Setyawan <49718582+adrianfathurs@users.noreply.github.com>
2025-09-25 12:40:13 +0700

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
   basePath: "/portofolio_online",
   images: {
     unoptimized: true,
