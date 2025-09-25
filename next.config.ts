ca241962596831d3c67e48ab242b0db99778fdb1
Adrian Fathur Setyawan <49718582+adrianfathurs@users.noreply.github.com>
2025-09-25 13:26:00 +0700

    n

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   basePath: "/portofolio_online",
+  assetPrefix: "/portofolio_online/",
   images: {
     unoptimized: true,
   },
